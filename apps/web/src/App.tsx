c43183557d813ddc7d1fc576e5cbcf4b92635589
KJH <kjhg5852@naver.com>
2021-11-02 15:34:24 +0900

    WEB: 메인 뷰 작업

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App">123</div>;
+enum ContentType {
+  MAIN = 'MAIN',
+  ABOUT = 'ABOUT',
+  CONTACT = 'CONTACT',
 }
+
+const App: React.FC = () => {
+  const [type, setType] = useState(ContentType.MAIN);
+
+  return (
+    <div className="container">
+      <div className="wrap">
+        <aside className="side"></aside>
+        <section className="content-wrap">
+          <article
+            className={type === ContentType.MAIN ? 'content show' : 'content'}
+          >
+            MAIN
+          </article>
+          <article
+            className={type === ContentType.ABOUT ? 'content show' : 'content'}
+          >
+            ABOUT
+          </article>
+          <article
+            className={
+              type === ContentType.CONTACT ? 'content show' : 'content'
+            }
+          >
+            CONTACT
+          </article>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default App;
