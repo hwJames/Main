ce459762caf5bf35dd4d4067e3d39f30ac5a6927
KJH <kjhg5852@naver.com>
2021-11-03 17:59:00 +0900

    WEB: 메인 뷰 작업 - 1
    화면 구조 수정중

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,39 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, WheelEvent } from 'react';
 import './App.css';
 
-enum ContentType {
-  MAIN = 'MAIN',
-  ABOUT = 'ABOUT',
-  CONTACT = 'CONTACT',
-}
-
 const App: React.FC = () => {
-  const [type, setType] = useState(ContentType.MAIN);
+  const [overScroll, setOverScroll] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
+
+  const pageCnt = 3;
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    window.scrollTo({ top: page * window.innerHeight, behavior: 'smooth' });
+  }, [page]);
 
   return (
-    <div className="container">
-      <div className="wrap">
-        <aside className="side"></aside>
-        <section className="content-wrap">
-          <article
-            className={type === ContentType.MAIN ? 'content show' : 'content'}
-          >
-            MAIN
-          </article>
-          <article
-            className={type === ContentType.ABOUT ? 'content show' : 'content'}
-          >
-            ABOUT
-          </article>
-          <article
-            className={
-              type === ContentType.CONTACT ? 'content show' : 'content'
-            }
-          >
-            CONTACT
-          </article>
-        </section>
-      </div>
+    <div
+      className="container"
+      onWheel={(e: WheelEvent<HTMLDivElement>) => {
+        if (!isScroll && e.deltaX === 0) {
+          if (page > 0 && e.deltaY < 0) {
+            setPage(page - 1);
+            console.log('up');
+          }
+          if (page < pageCnt - 1 && e.deltaY > 0) {
+            console.log('down');
+            setPage(page + 1);
+          }
+          if (e.deltaY !== 0) {
+            setIsScroll(true);
+
+            setTimeout(() => {
+              setIsScroll(false);
+            }, 1000);
+          }
+        }
+      }}
+    >
+      <div className="wrap">1</div>
+      <div className="wrap">2</div>
+      <div className="wrap">3</div>
     </div>
   );
 };
